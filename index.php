6d693eef45e56e9f42ae714dac8a5df262e5e5be
Matteo Bombardelli <mabom2007@gmail.com>
2024-10-17 10:28:41 +0200

    Projekt angelegt.

--- index.php
+++ index.php
@@ -1,35 +1,157 @@
 <?php
-require_once 'GradeModel.php';
 
+use models\GradeModel;
 session_start();
 
-if (!isset($_SESSION['gradeModel'])) {
-    $_SESSION['gradeModel'] = new GradeModel();
-}
+require_once "GradeModel.php";
 
-$gradeModel = $_SESSION['gradeModel'];
+$e = new GradeModel();
 $message = '';
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    if (isset($_POST['add'])) {
-        $studentName = htmlspecialchars($_POST['student']);
-        $email = htmlspecialchars($_POST['email']);
-        $subject = htmlspecialchars($_POST['subject']);
-        $grade = htmlspecialchars($_POST['grade']);
-        $examDate = htmlspecialchars($_POST['examDate']);
+//Überprüfen ob das Formular abgesendet wurde
+if (isset($_POST["submit"])) {
 
-        if ($gradeModel->addGrade($studentName, $email, $subject, $grade, $examDate)) {
-            $message = "Die eingegebenen Daten sind in Ordnung!";
-        } else {
-            $message = "Ungültige Note. Bitte eine Zahl zwischen 1 und 5 eingeben.";
+    $e->setName(isset($_POST["name"]) ? $_POST["name"] : "");
+    $e->setEmail(isset($_POST["email"]) ? $_POST["email"] : "");
+    $e->setExamDate(isset($_POST["examDate"]) ? $_POST["examDate"] : "");
+    $e->setGrade(isset($_POST["grade"]) ? $_POST["grade"] : "");
+    $e->setSubject(isset($_POST["subject"]) ? $_POST["subject"] : "");
+
+
+    if($e->validate()) {
+        $e->save();
+        $message = "<p class='alert alert-success'>Die eingegebenen Daten sind in Ordnung</p>";
+    } else {
+        $message = "<div class='alert alert-danger'><p>Die eingegebenen Daten sind fehlerhaft</p><ul>";
+
+        foreach ($e->getErrors() as $key => $value) {
+            $message .= "<li>" . $value . "</li>";
         }
-    } elseif (isset($_POST['delete'])) {
-        $gradeModel->deleteGrades();
-        $message = "Alle Noten wurden gelöscht.";
+        $message .= "</ul></div>";
     }
+
 }
 
-$grades = $gradeModel->getGrades();
 
-include 'view.php';
 ?>
+<!doctype html>
+<html lang="en" xmlns="http://www.w3.org/1999/html">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <title>Notenerfassung</title>
+
+    <script type="text/javascript" src="js/index.js"></script>
+</head>
+<body>
+<div class="container">
+
+    <h1 class="mt-5 mb-3">Notenerfassung 2.0</h1>
+
+    <?= $message ?>
+
+
+    <form id="form_grade" action="index.php" method="post">
+
+        <div class="row">
+            <div class="col-sm-6 form-group">
+                <label for="name">Name*</label>
+                <input type="text" name="name" class="form-control <?= $e->hasErrors('name') ? 'is-invalid' : '' ?>" required value="<?= htmlspecialchars($e->getName()) ?>">
+            </div>
+            <div class="col-sm-6 form-group">
+                <label for="email">E-Mail</label>
+                <input type="email" name="email" class="form-control <?= $e->hasErrors('email') ? 'is-invalid' : '' ?>" value="<?= htmlspecialchars($e->getEmail()) ?>">
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-sm-4 form-group">
+
+                <label for="subject">Fach*</label>
+                <select name="subject" class="custom-select <?= $e->hasErrors('subject') ? 'is-invalid' : '' ?>" required>
+                    <option value="" hidden="">- Fach auswählen -</option>
+                    <option value="m" <?php if ($e->getSubject() == 'm') echo "selected='selected'"; ?>>Mathematik</option>
+                    <option value="d" <?php if ($e->getSubject() == 'd') echo "selected='selected'"; ?>>Deutsch</option>
+                    <option value="e" <?php if ($e->getSubject() == 'e') echo "selected='selected'"; ?>>Englisch</option>
+                </select>
+            </div>
+
+            <div class="col-sm-4 form-group">
+                <label for="grade">Note*</label>
+                <input type="number" name="grade" class="form-control <?= $e->hasErrors('grade') ? 'is-invalid' : '' ?>" min="1" max="5" required value="<?= htmlspecialchars($e->getGrade()) ?>">
+            </div>
+
+            <div class="col-sm-4 form-group">
+                <label for="examDate">Prüfungsdatum*</label>
+                <input type="date" name="examDate" class="form-control <?= $e->hasErrors('examDate') ? 'is-invalid' : '' ?>"  onchange="validateExamDate(this)" required value="<?= htmlspecialchars($e->getExamDate()) ?>">
+            </div>
+        </div>
+
+        <div class="row mt-3">
+
+            <div class="col-sm-3 mb-3">
+                <input type="submit" name="submit" class="btn btn-primary btn-block" value="Speichern">
+            </div>
+
+            <div class="col-sm-3">
+                <a href="index.php" class="btn btn-secondary btn-block">Löschen</a>
+            </div>
+
+        </div>
+    </form>
+
+    <h2 class="mt-3">Noten</h2>
+    <div id="grades">
+
+        <table class="table">
+            <thead>
+            <tr>
+                <th>Name</th>
+                <th>E-Mail</th>
+                <th>Prüfungsdatum</th>
+                <th>Fach</th>
+                <th>Note</th>
+            </tr>
+            </thead>
+            <tbody>
+
+            <?php
+
+            $grade = GradeModel::getAll();
+
+            foreach ($grade as $g) {
+                echo "<tr>";
+                echo "<td>" . $g->getName() . "</td>";
+                echo "<td>" . $g->getEmail() . "</td>";
+                echo "<td>" . $g->getExamDateFormatted() . "</td>";
+                echo "<td>" . $g->getSubjectFormatted() . "</td>";
+                echo "<td>" . $g->getGrade() . "</td>";
+                echo "</tr>";
+            }
+
+            ?>
+
+            </tbody>
+        </table>
+
+    </div>
+
+    <?php
+    if(count($grade) > 0) {
+        ?>
+        <form action="clear.php" method="post">
+            <input type="submit" name="clear" class="btn btn-danger" value="Alle Noten löschen">
+        </form>
+        <?php
+    }
+    ?>
+
+
+</div>
+
+
+</body>
+</html>
